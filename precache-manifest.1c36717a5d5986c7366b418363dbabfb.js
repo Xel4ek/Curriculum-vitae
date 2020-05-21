c377a672df34487d7953474be5777a6d7afc28b5
Xel4ek <l-baal@yandex.ru>
2020-05-21 18:08:58 +0300

    v1

---
+++ precache-manifest.1c36717a5d5986c7366b418363dbabfb.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e1f38436e5512ffda155",
+    "url": "/css/app.4afa16d2.css"
+  },
+  {
+    "revision": "7a1a367fb27f1337eb3f",
+    "url": "/css/chunk-vendors.3b8adcd8.css"
+  },
+  {
+    "revision": "3740370d614f28e7baead968b03db24d",
+    "url": "/img/black-shape-paper-plane.3740370d.png"
+  },
+  {
+    "revision": "9416ae2794788f6bf7224309c290362e",
+    "url": "/img/coursera.9416ae27.svg"
+  },
+  {
+    "revision": "0a3d23185120a064e687c8ac46362884",
+    "url": "/img/logo.0a3d2318.svg"
+  },
+  {
+    "revision": "af5be6b765c4d059a67aae94bdd4be18",
+    "url": "/img/photo-4.af5be6b7.jpg"
+  },
+  {
+    "revision": "e4dd8825e1fd0d494baf1bce7644d6c6",
+    "url": "/img/stepik_logotype_blac.e4dd8825.svg"
+  },
+  {
+    "revision": "9deddc7b319e25bc8bc96aac987aa5c7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e1f38436e5512ffda155",
+    "url": "/js/app.a27c2871.js"
+  },
+  {
+    "revision": "7a1a367fb27f1337eb3f",
+    "url": "/js/chunk-vendors.cd0fe2e0.js"
+  },
+  {
+    "revision": "57ad5e9c4f3eaa6f30bc6bc92672b457",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/robots.txt"
+  }
+]);
